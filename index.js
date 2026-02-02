15f89dc5c0c941ecea525d9b3f11f1364b265ef7
vishakha <vishakha@gmail.com>
2026-02-02 15:46:16 +0530

    fix: allow admin origin in CORS configuration

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use(cookieParser())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://localhost:5174",
         "https://shoppilot-admin.vercel.app"
     ],
     credentials: true
