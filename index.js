241d384697596ef7c3cb9f39107861f3ea7e1db9
vishakha <vishakha@gmail.com>
2026-02-02 16:24:03 +0530

    fix: simplify CORS and remove invalid OPTIONS route

--- index.js
+++ index.js
@@ -32,11 +32,9 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("/*", cors());
+
 
 app.use(express.json())
 app.use(cookieParser())
